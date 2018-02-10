1a91617cec56b27546758dce0df52b1676b6d7da
abailly_acs <antonin.bailly@laposte.net>
2018-02-10 22:44:34 +0100

    users can post, no csrf

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,12 +6,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Entity\Post;
+
+use App\Form\PostType;
+
 class PostController extends Controller
 {
    /**
    * @Route("/posts/add", name="addPost")
    */
-   public function addPost() {
+   public function addPost(Request $request) {
 
      $this->denyAccessUnlessGranted('ROLE_USER', null, 'Unable to access this page!');
 
@@ -20,15 +26,24 @@ class PostController extends Controller
      $user = $this->getUser();
 
      $post = new Post;
-     $post->setUser($user);
-     $post->setDatetime(new \DateTime(date("Y-m-d")));
-     $post->setImg("img.png");
-     $post->setContent("This is a post");
+     $form = $this->createForm(PostType::class, $post);
+
+     $form->handleRequest($request);
+     if ($form->isSubmitted() && $form->isValid()) {
+
+       $em = $this->getDoctrine()->getManager();
+       $post->setDatetime(new \DateTime(date("Y-m-d")));
+       $post->setUser($user);
+       $em->persist($post);
+       $em->flush();
+
+       return $this->redirectToRoute('home');
+     }
 
      $em->persist($post);
      $em->flush();
 
-     return $this->redirectToRoute('home');
+    return new Response('osef');
 
   }
 
